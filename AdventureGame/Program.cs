b850ae1f0e223df07d4aa531ab67599eaa7729a6
Justina Vickers <justinav38@gmail.com>
2019-08-01 23:23:32 -0500

    initial commit

---
+++ AdventureGame/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace AdventureGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Adventure Game!");
+            Console.ReadKey();
+
+        }
+    }
+}
