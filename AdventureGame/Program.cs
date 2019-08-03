d2e703cbfbdbc305f7e46450c92e6c009159e550
Justina Vickers <justinav38@gmail.com>
2019-08-02 23:06:43 -0500

    refactored character name method

--- AdventureGame/Program.cs
+++ AdventureGame/Program.cs
@@ -2,19 +2,35 @@
 
 namespace AdventureGame
 {
+   public static class Game
+    {
+        static string CharacterName = "";
+
+        public static void StartGame()
+        {
+            Console.WriteLine("Hello!");
+            Console.WriteLine("Welcome to Middle Earth...");
+            NameCharacter();
+        }
+        static void NameCharacter()
+        {
+            Console.Write("What would you like your character's name to be?");
+            CharacterName = Console.ReadLine();
+
+            Console.WriteLine("Welcome, " + CharacterName + " and safe travels!");
+        }
+    }
+    class Item
+    {
+    }
+
+   
     class Program
     {
-        //string CharacterName = "";
         static void Main()        {
 
-            string CharacterName = "Lemony Snickett";
 
-            Console.WriteLine("Adventure Game!");
-            Console.WriteLine("Welcome to my game...");
-            Console.WriteLine("What would you like your character's name to be?");
-            CharacterName = Console.ReadLine();
-            Console.WriteLine("Great! Your character is now named " + CharacterName);
-
+            Game.StartGame();
             Console.Read();
 
         }
